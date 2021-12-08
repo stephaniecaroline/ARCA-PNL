7fef517b09df200abb3b57b90333bdb432c5f7af
Stephanie Caroline <54449173+stephaniecaroline@users.noreply.github.com>
2021-12-08 20:34:32 -0300

    Add files via upload

---
+++ matrizcov.m
@@ -0,0 +1,23 @@
+function [Q] = matrizcov(A)
+  [L,C] = size(A); 
+  X = zeros(C,1);
+  Q = zeros(C);
+  
+##  for i=1:C
+##    y = A(:,i);
+##    X(i) = var(y)*(L-1)/L;
+##  endfor
+  
+  for i = 1:C
+    for j = 1:C
+      if j>i
+        y = A(:,i);
+        z = A(:,j);
+        Q(i,j) = cov(y,z)*(L-1)/L;
+        Q(j,i) = Q(i,j);
+      elseif j==i
+        Q(i,j) = var(A(:,i))*(L-1)/L;
+      endif
+    endfor
+  endfor
+endfunction
